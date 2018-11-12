9fa336790483147570139f28ba6b1057a811f0c8
jeonjihun <nin1212@hufs.ac.kr>
2018-11-13 00:48:41 +0900

    add voting content

--- src/component/votingContent/VotingContent.js
+++ src/component/votingContent/VotingContent.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import "./VotingContent.css";
 
 class VotingContent extends Component {
 	render() {
 		return(
-			<div className="">
-				content
+			<div className="voting-content">
+				<div className="title text-left m-b-50 ">
+					선거이름
+				</div>
+				<div className="ui celled internally grid">
+					<div className="row">
+						<div className="four wide column">
+							photo
+						</div>
+						<div className="twelve wide column text-left">
+							<div className="voting-content-title">
+								선거기간
+							</div>
+							<div className="m-t-20 m-b-20">
+								2018/09/30 ~ 2018/10/07
+							</div>
+							<div className="voting-content-title">
+								선거내용
+							</div>
+							<div className="m-t-20 m-b-20">
+								100대 회장선거
+							</div>
+						</div>
+					</div>
+					<div className="row">
+
+					</div>
+				</div>
 			</div>
 			
 		)
